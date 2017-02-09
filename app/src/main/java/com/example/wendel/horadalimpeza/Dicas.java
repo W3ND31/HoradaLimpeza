0747c2d84120d3b7172abe79dae85ca41e2316c1
Wendel Macedo <wendelmacedooliveira@hotmail.com>
2017-02-09 08:32:25 -0300

    Terceiro Commit. Apenas organização de código. Mais organização será feita nos próximos dias (retirada de alguns comentários não necessários...)

--- app/src/main/java/com/example/wendel/horadalimpeza/Dicas.java
+++ app/src/main/java/com/example/wendel/horadalimpeza/Dicas.java
@@ -13,19 +13,17 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class Dicas extends AppCompatActivity {
-    //TextView content;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.dicas);
         ListView listView = (ListView)findViewById(android.R.id.list);
-        //content = (TextView)findViewById(R.id.single_list_item_label);
 
         // Guardando resources de String no Array
         String[] equips = getResources().getStringArray(R.array.equips);
 //        // Amarrando o Array no ListAdapter
+        // Documentar os parâmetros do setAdapter
         listView.setAdapter(new ArrayAdapter<String>(this,R.layout.list_item,R.id.label,equips));
 
 //
@@ -33,9 +31,6 @@ public class Dicas extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
 				String equip = ((TextView) view).getText().toString();
-//				Intent i = new Intent(getApplicationContext(),SingleListItem.class);
-//				i.putExtra("equip", equip);
-//				startActivity(i);
                 if (equip.equals("ArCondicionado")){
                     String dica = getString(R.string.ArCondicionado);
                     Intent i = new Intent(getApplicationContext(),DicaExib.class);
@@ -43,31 +38,26 @@ public class Dicas extends AppCompatActivity {
                     startActivity(i);
                 }else if(equip.equals("Geladeira")){
                     String dica = getString(R.string.Geladeira);
-                    //content.setText(dica);
                     Intent i = new Intent(getApplicationContext(),DicaExib.class);
                     i.putExtra("dica",dica);
                     startActivity(i);
                 }else if(equip.equals("Liquidificador")){
                     String dica = getString(R.string.Liquidificador);
-                    //content.setText(dica);
                     Intent i = new Intent(getApplicationContext(),DicaExib.class);
                     i.putExtra("dica",dica);
                     startActivity(i);
                 }else if(equip.equals("Microondas")){
                     String dica = getString(R.string.Microondas);
-                    //content.setText(dica);
                     Intent i = new Intent(getApplicationContext(),DicaExib.class);
                     i.putExtra("dica",dica);
                     startActivity(i);
                 }else if(equip.equals("Notebook")){
                     String dica = getString(R.string.Notebook);
-                    //content.setText(dica);
                     Intent i = new Intent(getApplicationContext(),DicaExib.class);
                     i.putExtra("dica",dica);
                     startActivity(i);
                 }else{
                     String dica = "Não Funcionou!";
-                    //content.setText(dica);
                     Intent i = new Intent(getApplicationContext(),DicaExib.class);
                     i.putExtra("dica",dica);
                     startActivity(i);
@@ -86,13 +76,4 @@ public class Dicas extends AppCompatActivity {
                 return super.onOptionsItemSelected(menuItem);
         }
     }
-//    @Override
-//    protected void onItemClick(listView l, View v, int position, long id){
-//        super.onListItemClick(l,v,position,id);
-//        int itemPosition = position;
-//        String itemValue = (String)l.getItemAtPosition(position);
-//
-
-////        Intent i = new Intent(this,DicaExib.class);
-//    }
 }
